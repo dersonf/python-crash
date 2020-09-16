c943c1797d7f1680506db8794e5bb1360dd64a34
Anderson Ferneda <nospam@bol.com.br>
2020-09-16 09:08:26 -0300

    capitulo 04

---
+++ capitulo04/exercicios/animals.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+animals = ['gato', 'cachorro', 'papagaio', 'periquito']
+for animal in animals:
+    print(f'Um {animal} seria um ótimo animal de estimação.')
+print(f"\nEste animais seriam otimos bichos de estimação!")
---
+++ capitulo04/exercicios/pizzas.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+pizzas = ['portuguesa', 'mussarela', 'marguerita']
+for pizza in pizzas:
+    print(f'I like {pizza} pizza.')
+print('I really love pizza!')
