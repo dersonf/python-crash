97eddef8dc281b022dc8e019ceea14dbffa9711e
Anderson Ferneda <nospam@bol.com.br>
2020-09-22 09:19:23 -0300

    Capitulo 04 - tuplas

---
+++ capitulo04/exercicios/buffet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+foods = ('arroz', 'feijão', 'sopa', 'macarrão', 'carne cozida')
+print("Cardápio original:")
+for food in foods:
+    print(food.title())
+
+foods = ('arroz', 'feijão', 'batata frita', 'frango assado', 'carne cozida')
+print("\nCardápio novo:")
+for food in foods:
+    print(food.title())
