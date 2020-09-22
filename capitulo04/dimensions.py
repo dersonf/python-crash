e1be8ce2755f1a9c54ec1e9f9e174e73fc7986b2
Anderson Ferneda <nospam@bol.com.br>
2020-09-22 09:30:06 -0300

    Capitulo 04

---
+++ capitulo04/dimensions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+dimensions = (200, 50)
+print("Dimensões originais:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nDimensões modificadas:")
+for dimension in dimensions:
+    print(dimension)
