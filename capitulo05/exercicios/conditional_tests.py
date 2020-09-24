a1086e92b9a703b4c43ebed4b4916e05337e72b1
Anderson Ferneda <nospam@bol.com.br>
2020-09-24 09:45:11 -0300

    Capitulo 05 - teste condicional

---
+++ capitulo05/exercicios/conditional_tests.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+car = 'subaru'
+pizza = 'portuguesa'
+carros = False
+idade = 40
+docker = True
+kubernetes = True
+python = False
+ansible = True
+filhos = ['gabriel', 'luiza']
+esposa = 'patricia'
+luiza = 15
+gabriel = 4
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nSua pizza favorita é portuguesa? Eu acho que sim.")
+print(pizza == 'portuguesa')
+
+print("\nSua pizza favorita é calabreza: Eu acho que não.")
+print(pizza == 'calabreza')
+
+print("\nVocê gosta de carros?")
+print(carros == True)
+
+print("\nVocê é maior de idade?")
+print(idade >= 18)
+
+print("\nVocê já pode ter carteirinha pra não pagar passagem?")
+print(idade >= 65)
+
+print("\nVocê domina python?")
+print(python == True)
+
+print("\nVocê domina docker?")
+print(docker == True)
+
+print("\nVocê domina kubernetes?")
+print(kubernetes == True)
+
+print("\nVocê domina ansible?")
+print(ansible == True)
+
+print("\nVocê tem uma filha chamada Luiza?")
+print('Luiza'.lower() in filhos)
+
+print("\nVocê tem dois filhos?")
+print(len(filhos) == 2)
+
+print("\nVocê tem um filhos chamado patricia?")
+print('patricia' in filhos)
+
+print("\nO nome da pizza é portuguesa?")
+print(pizza == 'portuguesa')
+
+print("\nO nome da pizza não é portugueza com z?")
+print(pizza != 'portugueza')
+
+print("\nO nome da esposa é PaTrIciA?")
+print('PaTrIciA'.lower() == esposa)
+
+print("\nSeus dois filhos são menores de idade?")
+print(luiza <= 18 and gabriel <= 18)
+
+print("\nGabriel tem 4 anos?")
+print(gabriel == 4)
+
+print("\nLuiza não tem 4 anos?")
+print(luiza != 4)
+
+print("\nLuiza é maior de 18 anos?")
+print(luiza > 18)
+
+print("\nLuiza tem idade igual ou superior a 15 anos?")
+print(luiza >= 15)
