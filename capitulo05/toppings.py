5184bdaa8c4b4fbcf8b759e939a861f1986a0039
Anderson Ferneda <nospam@bol.com.br>
2020-09-25 09:20:45 -0300

    Capitulo 05 - usando loops e if

--- capitulo05/toppings.py
+++ capitulo05/toppings.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-requested_topping = ['mushrooms', 'extra cheese']
+requested_toppings = ['mushrooms', 'extra cheese', 'green peppers']
 
-if 'mushrooms' in requested_topping:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_topping:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_topping:
-    print("Adding extra cheese.")
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
 
 print("\nFinished making your pizza!")
