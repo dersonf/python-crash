42d06c5b3d4c0b856636f3fa31b7ab0e224a2509
Anderson Ferneda <nospam@bol.com.br>
2020-09-25 08:32:18 -0300

    Capitulo 05 - Muitos elif

--- capitulo05/amusement_park.py
+++ capitulo05/amusement_park.py
@@ -3,9 +3,13 @@
 age = 12
 
 if age < 4:
-    print("Your admission cost is $0.")
+    price = 0
 elif age < 18:
-    print("Your admission cost is $25.")
+    price = 25
+elif age < 65:
+    price = 40
 else:
-    print("Your admission cost is $40.")
+    price = 20
+
+print(f"Your admission cost is ${price}.")
 
