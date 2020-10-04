8d50d02845d8fcc937b3e5b09eb8cbaab118d63d
Anderson Ferneda <aferneda@uolinc.com>
2020-10-04 12:26:19 -0300

    Capitulo 07 - input

---
+++ capitulo07/exercicios/multiples_of_ten.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+numero = int(input("Informe um número: "))
+
+if numero % 10 == 0:
+    print(f"{numero} é multiplo de 10.")
+else:
+    print(f"{numero} não é multiplo de 10.")
