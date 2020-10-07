5b48262982582eae923d22be9496c77b2650c15e
Anderson Ferneda <nospam@bol.com.br>
2020-10-07 09:06:31 -0300

    Capitulo 08 - funções - argumentos mistos

--- capitulo08/pets.py
+++ capitulo08/pets.py
@@ -6,4 +6,5 @@ def describe_pet(pet_name, animal_type='dog'):
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet(pet_name='willie')
+describe_pet('willie')
+describe_pet('nadine', 'snake')
