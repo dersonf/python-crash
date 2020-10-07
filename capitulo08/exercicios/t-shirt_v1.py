370e97dec85a991232cf36df7913b8d09a12f09a
Anderson Ferneda <nospam@bol.com.br>
2020-10-07 09:27:03 -0300

    Capitulo 08 - funções - exercícios

---
+++ capitulo08/exercicios/t-shirt_v1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def make_shirt(tamanho, frase):
+    """Exibe o tamanho e a mensagem na camiseta."""
+    print(f"\nA camiseta tamanho {tamanho.upper()} tem a frase:")
+    print(frase)
+
+make_shirt('m', 'Farol alta na cara é igual mulher gritando no ouvido.')
+make_shirt(
+    tamanho='m',
+    frase='Farol alta na cara é igual mulher gritando no ouvido.',
+    )
+make_shirt(
+    frase='Farol alta na cara é igual mulher gritando no ouvido.',
+    tamanho='m',
+    )
---
+++ capitulo08/exercicios/t-shirt_v2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def make_shirt(tamanho='G', frase='I amo Pyhton'):
+    """Exibe o tamanho e a mensagem na camiseta."""
+    print(f"\nA camiseta tamanho {tamanho.upper()} tem a frase:")
+    print(frase)
+
+make_shirt()
+make_shirt('M')
+make_shirt('M', 'Eu amo minha esposa e filhos.')
