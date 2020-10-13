215920a5ffb6ac1f32c0f2b3fa3bf17f0b7eca7e
Anderson Ferneda <nospam@bol.com.br>
2020-10-13 09:16:47 -0300

    Capitulo 08 - exercicios

---
+++ capitulo08/making_pizzas.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#import pizza
+#from pizza import make_pizza
+from pizza import make_pizza as mp
+
+#pizza.make_pizza(16, 'pepperoni')
+#pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+#make_pizza(16, 'pepperoni')
+#make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
--- capitulo08/pizza.py
+++ capitulo08/pizza.py
@@ -1,12 +1,5 @@
-#!/usr/bin/python3
-
-
 def make_pizza(size, *toppings):
     """Print the list of toppings that have been requested."""
     print(f"\nMaking a {size}-inch pizza with a following toppings:")
     for topping in toppings:
         print(f"- {topping}")
-
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
