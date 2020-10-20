655eab215fb37570aa257b14f5387ddbf7433aaf
Anderson Ferneda <nospam@bol.com.br>
2020-10-20 08:40:56 -0300

    Capitulo 09 - exercicios classes e subclasses

--- capitulo09/car.py
+++ capitulo09/car.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+"""A set of classes used to represent gas and eletric cars."""
 
 
 class Car:
-    """Asimple attempt to represent a car."""
+    """A simple attempt to represent a car."""
 
     def __init__(self, make, model, year):
         """Initialize attibutes to describe a car."""
@@ -34,18 +34,38 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-#my_new_car = Car('audi', 'a4', 2019)
-#print(my_new_car.get_descriptive_name())
-#my_new_car.odometer_reading = 23
-#my_new_car.update_odometer(23)
-#my_new_car.update_odometer(25)
-#my_new_car.read_odometer()
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attibutes."""
+        self.battery_size = battery_size
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
+
+
+class ElectricCar(Car):
+    """Represent aspects of car, specific to eletric vehicle."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """Eletric cars don't have gas tanks."""
+        print("This car doens't need a gas tank!")
