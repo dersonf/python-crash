185091ab491dc14033f5b099a982812450600596
Anderson Ferneda <aferneda@uolinc.com>
2020-10-25 14:47:19 -0300

    Capitulo 10 - try exeption else

--- capitulo10/file_reader.py
+++ capitulo10/file_reader.py
@@ -3,13 +3,7 @@
 filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
-    #v1.0 contents = file_object.read()
-    #v1.2 for line in file_object:
     lines = file_object.readlines()
-        #print(line)
-        #v1.2 print(line.rstrip())
-#v1.0 print(contents)
-#v1.1 print(contents.rstrip())
 
 for line in lines:
     print(line.rstrip())
