116263650c6eecdda35d02213363c5e594ef00d0
Anderson Ferneda <aferneda@uolinc.com>
2020-10-25 15:20:24 -0300

    capitulo 10 - try except

---
+++ capitulo10/alice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+filename = 'alice.txt'
+
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the {filename} does not exist.")
