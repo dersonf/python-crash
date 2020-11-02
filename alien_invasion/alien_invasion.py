406abda91307b43e604413f16761d912ae12cbdd
Anderson <anderson@pinguim.local>
2020-11-01 23:27:17 -0300

    Movement do left

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -37,16 +37,21 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship  to the right.
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
