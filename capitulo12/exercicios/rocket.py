580dc315949a73a5b4c7fe976c43221f01c825e6
Anderson <anderson@pinguim.local>
2020-11-04 08:25:12 -0300

    Moving ship

--- capitulo12/exercicios/rocket.py
+++ capitulo12/exercicios/rocket.py
@@ -23,11 +23,31 @@ class Rocket:
             pygame.display.flip()
             self.screen.blit(self.backgroud, [0, 0])
             self.rocket.blitme()
+            self.rocket.update()
 
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.rocket.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.rocket.moving_down = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.rocket.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.rocket.moving_down = False
+
 
 if __name__ == '__main__':
     rocket = Rocket()
--- capitulo12/exercicios/ship.py
+++ capitulo12/exercicios/ship.py
@@ -12,5 +12,22 @@ class Ship:
         self.rect = self.image.get_rect()
         self.rect.center = self.screen_rect.center
 
+        # Moviment flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update ship position."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+        if self.moving_up:
+            self.rect.y -= 1
+        if self.moving_down:
+            self.rect.y += 1
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
