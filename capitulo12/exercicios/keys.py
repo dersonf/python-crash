20e2420f2ee3245d0585385cf840cb2ae75d8dfd
Anderson <anderson@pinguim.local>
2020-11-05 09:43:00 -0300

    show keys

--- capitulo12/exercicios/keys.py
+++ capitulo12/exercicios/keys.py
@@ -1,5 +1,30 @@
 import pygame
 import sys
+from keys_settings import Settings
+
+class Text:
+    """A class to format and show a text."""
+
+    def __init__(self, k_game):
+        self.default_text = 'Press any key'
+        self.screen = k_game.screen
+        self.settings = k_game.settings
+        self.screen_rect = k_game.screen.get_rect()
+        self.text_color = (0, 0, 0)
+        self.font = pygame.font.SysFont('Bitstream Charter', 30)
+        self.text = self.font.render(self.default_text, True, self.text_color)
+        self.textRect = self.text.get_rect()
+        self.textRect.center = (
+            self.settings.screen_height // 2, self.settings.screen_width // 2
+        )
+
+    def update(self, event):
+        """Update text info."""
+        self.default_text = event
+
+    def blitme(self):
+        self.screen.blit(self.text, self.textRect)
+
 
 
 class KeysPressed:
@@ -8,31 +33,38 @@ class KeysPressed:
     def __init__(self):
         """Initialize the key pressed viewer."""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((800, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Show presses Keys")
         self.bg_color = (200, 200, 200)
-        self.font = pygame.font.SysFont('Bitstream Charter', 30)
+        self.text = Text(self)
 
     def run_keys(self):
         """Start the main loop."""
         while True:
             self._check_events()
             self._update_screen()
+            # self.text.update()
             pygame.display.flip()
-
-    def print_event_key(self):
-        self.text = Texto
-
 
     def _update_screen(self):
         """Update images"""
         self.screen.fill(self.bg_color)
+        self.text.blitme()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self.pressed_key = pygame.key.get_pressed()
+                print(self.pressed_key)
+                if event.key == pygame.K_q:
+                    sys.exit()
+                elif self.pressed_key:
+                    self.text.update(self.pressed_key)
+                    # self.text.default_text = event.key
 
 if __name__ == '__main__':
     keys = KeysPressed()
---
+++ capitulo12/exercicios/keys_settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """Class to store settings for keys.py."""
+
+    def __init__(self):
+        """Initializing class"""
+        self.screen_width = 800
+        self.screen_height = 800
