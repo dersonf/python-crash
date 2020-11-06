7d052b410f272aedebcf6faa79fc5badaa8dc65c
Anderson <anderson@pinguim.local>
2020-11-06 09:47:46 -0300

    Ainda não funciona

--- capitulo12/exercicios/keys.py
+++ capitulo12/exercicios/keys.py
@@ -2,28 +2,29 @@ import pygame
 import sys
 from keys_settings import Settings
 
-class Text:
-    """A class to format and show a text."""
+# class Text:
+#     """A class to format and show a text."""
 
-    def __init__(self, k_game):
-        self.default_text = 'Press any key'
-        self.screen = k_game.screen
-        self.settings = k_game.settings
-        self.screen_rect = k_game.screen.get_rect()
-        self.text_color = (0, 0, 0)
-        self.font = pygame.font.SysFont('Bitstream Charter', 30)
-        self.text = self.font.render(self.default_text, True, self.text_color)
-        self.textRect = self.text.get_rect()
-        self.textRect.center = (
-            self.settings.screen_height // 2, self.settings.screen_width // 2
-        )
+#     def __init__(self, k_game):
+#         self.default_text = 'Press any key'
+#         self.screen = k_game.screen
+#         self.settings = k_game.settings
+#         self.screen_rect = k_game.screen.get_rect()
+#         self.text_color = (0, 0, 0)
+#         self.font = pygame.font.SysFont('Bitstream Charter', 30)
+#         self.text = self.font.render(self.default_text, True, self.text_color)
+#         self.textRect = self.text.get_rect()
+#         self.textRect.center = (
+#             self.settings.screen_height // 2, self.settings.screen_width // 2
+#         )
 
-    def update(self, event):
-        """Update text info."""
-        self.default_text = event
+#     def update(self, event):
+#         """Update text info."""
+#         # self.default_text = event
+#         self.text = self.font.render(event, True, self.text_color)
 
-    def blitme(self):
-        self.screen.blit(self.text, self.textRect)
+#     def blitme(self):
+#         self.screen.blit(self.text, self.textRect)
 
 
 
@@ -36,9 +37,10 @@ class KeysPressed:
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width,self.settings.screen_height))
-        pygame.display.set_caption("Show presses Keys")
+        pygame.display.set_caption("Show pressed Key")
         self.bg_color = (200, 200, 200)
-        self.text = Text(self)
+        self.screen_rect = self.screen.get_rect()
+        # self.text = Text(self)
 
     def run_keys(self):
         """Start the main loop."""
@@ -51,23 +53,26 @@ class KeysPressed:
     def _update_screen(self):
         """Update images"""
         self.screen.fill(self.bg_color)
-        self.text.blitme()
+        # Text message
+        self.msg = "Press any key."
+        self.msg_color = (0, 0, 0)
+        self.msg_bg_color = (200, 200, 200)
+        self.f = pygame.font.SysFont(None, 48)
+        self.msg_image = self.f.render(self.msg, True, self.msg_color, self.msg_bg_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.screen_rect.center
+        self.screen.blit(self.msg_image, self.msg_image_rect)
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                # self.pressed_key = pygame.key.get_pressed()
-                # print(self.pressed_key)
                 if event.key == pygame.K_q:
                     sys.exit()
-                # elif self.pressed_key:
-                #     self.text.update(self.pressed_key)
-                    # self.text.default_text = event.key
-        for k, p in enumerate(pygame.key.get_pressed()):
-            if p:
-                self.text.update(pygame.key.name(k))
+                else:
+                     print(event.key)
+
 
 if __name__ == '__main__':
     keys = KeysPressed()
