bbf5bc38fedf12b0f621989fd33bd8d282966037
Anderson <anderson@pinguim.local>
2020-11-16 09:50:38 -0300

    Adding start game e text box.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,9 +2,12 @@
 
 import sys
 from time import sleep
+
 import pygame
+
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -33,6 +36,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # Make the Play buton.
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -88,6 +94,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Make the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
