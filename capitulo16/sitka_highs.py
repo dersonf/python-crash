321fc7a4ad86c55a3bde485c050cd59d65069224
Anderson <anderson@pinguim.local>
2020-12-02 09:31:13 -0300

    Temperature analyse

--- capitulo16/sitka_highs.py
+++ capitulo16/sitka_highs.py
@@ -1,7 +1,34 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    # print(header_row)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    # Get high temperatures from this file.
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+# print(highs)
+
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+# Change just the font size
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
