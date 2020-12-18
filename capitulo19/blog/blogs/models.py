f58d519b406a4d212932304810c2d7b18c32dab5
Anderson <anderson@pinguim.local>
2020-12-18 09:36:04 -0300

    A new project as an exercise - blogs

---
+++ capitulo19/blog/blogs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class BlogPost(models.Model):
+    """A post in the blog."""
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.title}"
