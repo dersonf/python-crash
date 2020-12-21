c90f357603a31bf2b88ab34b0ea2db9f918b95da
Anderson <anderson@pinguim.local>
2020-12-21 09:51:15 -0300

    A new project as an exercise - blogs

--- capitulo19/blog/blogs/views.py
+++ capitulo19/blog/blogs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import BlogPost
+
+
+def index(request):
+    """The home page of Blogs."""
+    return render(request, 'blogs/index.html')
+
+
+def posts(request):
+    """Return the main posts page."""
+    posts = BlogPost.objects.order_by('-date_added')
+    context = {'posts': posts}
+    return render(request, 'blogs/posts.html', context)
+
